89a8b74348acf7d73cb78796fa206c54b3ea49cd
dulguuntam1r <46316929+dulguuntam1r@users.noreply.github.com>
2022-10-10 23:44:53 +0800

    Add files via upload

---
+++ 1NameGenerator.py
@@ -0,0 +1,7 @@
+print("Hello there!\nI will help you with your band name by asking 2 questions :)")
+
+city_name = input("Which city did you grow up in?\n")
+
+pet_name = input("What is name of your pet?\n")
+
+print("Your band name is: " + pet_name + " " +city_name)
