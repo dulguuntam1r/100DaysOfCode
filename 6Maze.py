6159205a7f71ed504ac240768d890fe33035d544
dulguuntam1r <46316929+dulguuntam1r@users.noreply.github.com>
2022-10-17 23:25:29 +0800

    Add files via upload

---
+++ 6Maze.py
@@ -0,0 +1,19 @@
+#Sixth day challenge
+#Reeborg game at https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def find_way():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear() and wall_on_right():
+        move()
+    elif wall_in_front() and wall_on_right():
+        turn_left()
+    
+
+while not at_goal():
+    find_way()
